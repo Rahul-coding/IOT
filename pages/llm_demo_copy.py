61d95da677a2c140775386641851eb59abe8ee41
Rahul-coding <95549095+Rahul-coding@users.noreply.github.com>
2023-11-24 04:51:24 +0000

    toggle added

--- pages/llm_demo_copy.py
+++ pages/llm_demo_copy.py
@@ -1,26 +1,23 @@
-
-
-import cohere
-import os
 import streamlit as st
 import streamlit_toggle as tog
-on = st.toggle('Use backup file')
+import cohere 
+import os
 co = cohere.Client(os.environ.get('COHERE_KEY'))
-control =0 
+control = 0
+on = st.toggle('Use backup file')
 with open('./parsed_data/cheezit.txt') as data:
   control = 1
-st.title("LLM - Cohere")
+control = 1
+
 
 if st.button('analyze'):
     if on:
         response = co.generate(
   prompt= "Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format" + str(data))
-        st.write(response.generations[0].text)
+        print(response.generations[0].text)
     else:
          response = co.generate(
   prompt= "Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format" + str(data))
          
 else:
     st.write("Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format + str(data)")
-
-
