cf3f84132cecc29ca357903208fa6eb698296a9b
Rahul-coding <95549095+Rahul-coding@users.noreply.github.com>
2023-11-24 22:11:38 +0000

    real files test

--- pages/llm_demo.py
+++ pages/llm_demo.py
@@ -3,26 +3,38 @@
 import cohere
 import os
 import streamlit as st
+from io import StringIO
 
 on = st.toggle('Use backup file')
 co = cohere.Client(os.environ.get('COHERE_KEY'))
-control =0 
-with open('./parsed_data/real.txt') as data:
-  control = 1
-st.title("LLM - Cohere")
+data = ""
+st.title("Ranking")
+st.write("**Using Coheres Large Language Model**")
 
+text = "Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format: " + str(data)
+file_name = './parsed_data/real.txt'
+
+if on:
+  file_name = './parsed_data/backup.txt'
+
+file=open(file_name ,"r")
+data = file.read()
+st.subheader("Prompt")
+st.write(text+data)
+text+= data    
 if st.button('analyze'):
     if on:
         response = co.generate(
-  prompt= "Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format" + str(data))
+  prompt= text
+        )
+        st.subheader("Response")
         st.write(response.generations[0].text)
     else:
-         with open('./parsed_data/backup.txt') as data:
-             control = 1
          response = co.generate(
-  prompt= "Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format" + str(data))
-         
-else:
-    st.write("Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format")
+  prompt=text
+         )
+         st.subheader("Response")
+         st.write(response.generations[0].text)
+
 
 
