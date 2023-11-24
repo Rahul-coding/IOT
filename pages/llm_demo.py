461b8c718840e0e603bb9cc8fa9a3912e767b227
Rahul-coding <95549095+Rahul-coding@users.noreply.github.com>
2023-11-24 03:12:18 +0000

    cohere test

---
+++ pages/llm_demo.py
@@ -0,0 +1,20 @@
+
+
+import cohere
+import os
+import streamlit as st
+co = cohere.Client(os.environ.get('COHERE_KEY'))
+control =0 
+with open('/workspaces/stem-fair/parsed_data/cheezit.txt') as data:
+  control = 1
+st.title("LLM - Cohere")
+response = co.generate(
+  prompt= "Rank only the ingreditens in this JSON on a scale from 1-10, 10 being the healthiest.  Don't rank anything other than ingredients. give every ingredinet a ranking and return in a JSON format" + str(data))
+print("________________________________________________________________")
+print(response)
+st.title("Prompt")
+st.text(response.prompt)
+st.title("Response")
+st.text(response.generations[0].text)
+
+
