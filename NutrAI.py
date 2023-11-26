d18591e30035bda5242ee534f47d28ee534c0052
Rahul-coding <95549095+Rahul-coding@users.noreply.github.com>
2023-11-26 20:19:31 +0000

    phone test & added parse printing for de-bugging

--- NutrAI.py
+++ NutrAI.py
@@ -44,14 +44,15 @@ if st.button('analyze'):
         for match in expression.find(json_ocr):
             parsed += match.value  
 
-        ingredient_prompt = "give every ingredeient in this list a score from 1-10 with 10 being the helathiest. Only return the score. Give each ingredient an indivdual score  from 1-10. Return in a list format. Give sugary, high carb, and artificaly flavored food, items lower rankings. Rank natural and organic foods higher. Here are the ingredients:" + str(parsed)
-        overall_prompt = "give an overall health ranking for a food product containing these ingredients. Rank on a scale from 1-10 with 10 being the healthiest. Only return the ranking and no explanation. Return the score in a bigger font than everything else. Give sugary, high carb, and artificaly flavored food, items lower rankings. Rank natural and organic foods higher."+str(parsed)
+        ingredient_prompt = "give every ingredeient in this list a score from 1-10 with 10 being the helathiest. Only return the score. Give each ingredient an indivdual score  from 1-10. Return in a list format. Give sugary, high carb, and artificaly flavored food, and processed food, items significantly lower rankings. Rank natural and organic foods higher. Here are the ingredients:" + str(parsed)
+        overall_prompt = "give an overall health ranking for a food product containing these ingredients. Rank on a scale from 1-10 with 10 being the healthiest. Only return the ranking and no explanation. Return the score in a bigger font than everything else. Give sugary, high carb, and artificaly flavored food, and processed food items significantly lower rankings. Rank natural and organic foods higher."+str(parsed)
         print("__________")
         print(parsed)
         print("__________")
         st.text("**parsed**")
         st.text(parsed)
         
+        st.text(overall_prompt)
         st.text("Getting overall ranking")
         response = co.generate(prompt=overall_prompt)        
         st.subheader("Overall Ranking")
