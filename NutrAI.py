1a6905e269604cd7c18e1c02fd0d1da21fe1866f
Rahul-coding <95549095+Rahul-coding@users.noreply.github.com>
2023-11-26 19:56:44 +0000

    file save fix

--- NutrAI.py
+++ NutrAI.py
@@ -7,9 +7,12 @@ import cohere
 import os
 from io import StringIO
 
+co = cohere.Client(os.environ.get('COHERE_KEY'))
+
+
 st.title("NutrAI")
 st.text("press on the sqaure to take a photo")
-image = st.camera_input("Take a picture of food label")
+image = st.camera_input(label= "Take a picture of food label")
 if image:
     with open('./images/real.jpg', 'wb') as file: #saving as file 
         file.write(image.getvalue())
@@ -17,58 +20,43 @@ if image:
     st.text("Photo that was taken (new photo will override last one)")
     st.image('./images/real.jpg') # showing the image
     print('file save')  
-image = "./images/real.jpg"
-
-data = 0
-parsed = ''
+image = './images/real.jpg'
 
 
 
-
-
-co = cohere.Client(os.environ.get('COHERE_KEY'))
-data = ""
-
-
-file_name = './text/real.txt'
-
-
-file=open(file_name ,"r")
-data = file.read()
-
-if st.button('analyze', key = 'a'):
-        with open('./text/real.txt', 'w') as save:
-                save.write(str(parsed))  
-        
-        image = "./images/backup.jpg" 
-            #st.image('./images/backup.jpg')
+if st.button('analyze'):
+           
         st.text("Sending to OCR")
-        data = analyze(image)   
+        ocr = analyze(image)   
+        print("__________")
+        print(ocr)
+        print("__________")
         with open('./text/real.txt','w') as extract:
-            extract.write(str(data))
+            extract.write(str(ocr))
 
         with open('./text/real.txt', 'r') as json_file:
-            json_data = json.load(json_file)
+            json_ocr= json.load(json_file)
 
         expression = parse('pages[*].lines[*].text')
-        match = expression.find(json_data)
+        match = expression.find(json_ocr)
 
-        for match in expression.find(json_data):
+        parsed=""
+        for match in expression.find(json_ocr):
             parsed += match.value  
-                #st.image('./images/backup.jpg')
-        with open('./text/real.txt', 'w') as save:
-            save.write(str(parsed))  
-            text = "give every ingredeient in this list a score from 1-10 with 10 being the helathiest. Only return the score. Give each ingredient an indivdual score  from 1-10. Return in a list format. Give sugary, high carb, and artificaly flavored food, items lower rankings. Rank natural and organic foods higher. Here are the ingredients:" + str(parsed)
-            print(text)
-        first_text = "give an overall health ranking for a food product containing these ingredients. Rank on a scale from 1-10 with 10 being the healthiest. Only return the ranking and no explanation. Return the score in a bigger font than everything else. Give sugary, high carb, and artificaly flavored food, items lower rankings. Rank natural and organic foods higher."+str(parsed)
+
+        ingredient_prompt = "give every ingredeient in this list a score from 1-10 with 10 being the helathiest. Only return the score. Give each ingredient an indivdual score  from 1-10. Return in a list format. Give sugary, high carb, and artificaly flavored food, items lower rankings. Rank natural and organic foods higher. Here are the ingredients:" + str(parsed)
+        overall_prompt = "give an overall health ranking for a food product containing these ingredients. Rank on a scale from 1-10 with 10 being the healthiest. Only return the ranking and no explanation. Return the score in a bigger font than everything else. Give sugary, high carb, and artificaly flavored food, items lower rankings. Rank natural and organic foods higher."+str(parsed)
+        print("__________")
+        print(parsed)
+        print("__________")
+        
         st.text("Getting overall ranking")
-        response = co.generate(
-  prompt=first_text)        
+        response = co.generate(prompt=overall_prompt)        
         st.subheader("Overall Ranking")
         st.write(response.generations[0].text)
+        
         st.text("Getting individual rankings")
-        response = co.generate(
-  prompt=text)  
+        response = co.generate(prompt=ingredient_prompt)  
         st.subheader('Individual rankings')
         st.write(response.generations[0].text)
 
