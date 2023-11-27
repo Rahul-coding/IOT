0fb93c2ba90ecbcab1f2deb26e4f3cb8816e4ede
Rahul-coding <95549095+Rahul-coding@users.noreply.github.com>
2023-11-27 00:19:51 +0000

    tokens ran out but final edits

--- NutrAI.py
+++ NutrAI.py
@@ -40,12 +40,12 @@ if st.button('analyze'):
         parsed=""
         for match in expression.find(json_ocr):
             parsed += match.value  
+        print(ocr)
         print(parsed)
 
-        ingredient_prompt = '''you are a nutiritionist. your job is to examine a list of ingredients and rank them on a scale of 1-10, 10 being the most healthy. Your response should strictly be in the followding format. do not include anythinig else in your response.
-Ignore any words in input before ingrediants. Rank every ingredient in the folowing format:
-
-"ranking" | [['"ingredient'1",score1], ["ingredient2'"score2], ["ingredient3",score3]] "|" <overallscore>
+        ingredient_prompt = ''''you are a nutiritionist. your job is to examine a list of ingredients and rank them on a scale of 1-10, 10 being the most healthy. Your response should strictly be in the followding format. do not include anythinig else in your response. Add more elements to the array as needed to accomodate all ingredients in the input. Enclose the ingredient names in your response in single quotes.
+Ignore any words in input before ingrediants. Rank every ingredient in the folowing format: 
+"ranking" | [['ingredient'1',score1], ['ingredient2'score2], ['ingredient3',score3]] "|" <overallscore>
 ''' + str(parsed)
         overall_prompt = '''you are a nutiritionist. you job is to examine a list of ingredients and rank them on a scale of 1-10, 10 being the most healthy. Your response should strictly be in the followding format. do not include anythinig else in your response.  Score is the numerial score, reason is one sentense description. 
 Ignore any words in input before ingrediants.
@@ -56,30 +56,39 @@ Reason: <one sentence explaining your score"
 
 input
 ''' + str(parsed)
+        print(overall_prompt)
         
         print("__________")
         print(parsed)
         print("__________")
+        if parsed != "":
+            st.text("Getting overall ranking")
+            response = co.generate(prompt=overall_prompt,
+                                    max_tokens=300, temperature=0)        
+            string = response.generations[0].text   
+            score = string.split()[1]
+            reason = string.split(': ')[2]
+            score = score
+            st.subheader("Overall Ranking: "+str(score))
+
+            print(reason)
+
+            
+            st.text("Getting individual rankings")
+            response = co.generate(prompt=ingredient_prompt,
+                                    max_tokens=300, temperature=0) 
+            
+            st.subheader('Individual rankings')
+            ingredients= response.generations[0].text
+
+            print(response.generations[0].text)
+            split_ingredients = ingredients.split("|")[1]
+            in1 = eval(split_ingredients)
+            st.dataframe(data = in1, hide_index = True, column_config = {"0":"ingredient", "1": st.column_config.ProgressColumn("score", min_value = 0, max_value = 10, format= "%d") })
+            
+        else:
+            st.text("Sorry there was an error please try taking a more clear photo and try again")
+       
+                 
+
         
-        st.text("Getting overall ranking")
-        response = co.generate(prompt=overall_prompt)        
-        string = response.generations[0].text   
-        score = string.split()[1]
-        reason = string.split(': ')[2]
-        score = score
-        st.subheader("Overall Ranking: "+str(score))
-
-        print(reason)
-
-        
-        st.text("Getting individual rankings")
-        response = co.generate(prompt=ingredient_prompt) 
-        
-        st.subheader('Individual rankings')
-        ingredients= response.generations[0].text
-
-        print(response.generations[0].text)
-        split_ingredients = ingredients.split("|")[1]
-        in1 = eval(split_ingredients)
-        st.dataframe(data = in1, hide_index = True, column_config = {"0":"ingredient", "1": st.column_config.ProgressColumn("score", min_value = 0, max_value = 10, format= "%d") })
-
--- ingredienttest.py
+++ ingredienttest.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-ingredients  = "ranking | [['Rice Flour',1], ['Sugar',2], ['Baking Soda',3], ['Sodiumaluminum Phosphate',3], ['Monocalciumphosphate',3], ['Modified Potato Starch',4], ['Salt',5], ['Xanthan Gum',7]] | 5"
+ingredients  = " Ranking | [['Romaine Lettuce',5], ['Cucumber',4], ['Grape Tomatoes',4], ['Hard-cooked Eggs',6], ['Red Bell Pepper',5], ['Carrot',3], ['Cheddar Cheese',6], [Pasteurized Milk Cultures,1], [Salt,1], [Enzymes,1], [Annatto,1], [Potato,2], [Pepper,1], [Powered Cellulose,1], [Starch,1], [Natamycin,1]] | 8 "
 split_ingredients = ingredients.split("|")[1]
 in1 = eval(split_ingredients)
 st.dataframe(data = in1, hide_index = True, column_config = {"0":"ingredient", 
